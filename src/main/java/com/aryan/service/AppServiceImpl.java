49b92bba38bac6e3cddf33133ed8b4de2291c9f6
Aryan Kumar Shaw Halwai <135535751+aryanshaw616@users.noreply.github.com>
2025-02-27 21:58:00 +0530

    Again changes

--- src/main/java/com/aryan/service/AppServiceImpl.java
+++ src/main/java/com/aryan/service/AppServiceImpl.java
@@ -49,7 +49,8 @@ public class AppServiceImpl implements AppService {
 		urlDataRepo.save(data);
 
 
-		return "https://urlshortner-production-07b3.up.railway.app/tiny/" + unique;
+		return "https://UrLShortner.up.railway.app/tiny/" + unique;
+
 
 
 
